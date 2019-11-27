e2310e4e2b8dd8d7fb05e64b5759f97e590c2558
horie1024 <ryosuke.pic@gmail.com>
2019-11-27 14:11:29 +0900

    first commit.

---
+++ app/src/main/java/com/horie1024/mylibrarysample/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.horie1024.mylibrarysample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.horie1024.mylibrary.Hello
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val hello = Hello()
+        hello_world.text = hello.world("Horie")
+    }
+}
---
+++ mylibrary/src/main/java/com/horie1024/mylibrary/Hello.kt
@@ -0,0 +1,6 @@
+package com.horie1024.mylibrary
+
+class Hello {
+
+    fun world(name: String) = "Hello World ${name}!"
+}
